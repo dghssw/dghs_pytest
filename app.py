1429f752752f4263a0873d250a04af7828e004e0
dghssw <dghs-sw@pess.cnehs.kr>
2025-05-31 15:49:33 +0900

    외부서버 오픈

--- app.py
+++ app.py
@@ -32,4 +32,6 @@ def index():
     return render_template("index.html", question=code, answer=answer)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
+
+
